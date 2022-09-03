476a19e046424a1427be35402061ae267c9120d5
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-03 23:36:12 +0300

    решение задачи на квадрат числа

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// 1. принимает число 
+// 2. выдаёт его квадрат (число
+// умноженное на само себя).
+// 3. вывод результата
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int squear = num * num;
+Console.WriteLine($"Квадрат числа {num} = {squear} ");
