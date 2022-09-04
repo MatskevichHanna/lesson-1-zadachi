c5c3290a3f79d6f82457416cec4fd45414063142
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-04 23:51:29 +0300

    задача на четное число

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.WriteLine($"Число {num1} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не является четным");
+}
