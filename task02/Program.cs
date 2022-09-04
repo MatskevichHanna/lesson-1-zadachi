18cf56d3bb74d16875022dc1591db0d59cf6af9e
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-04 22:49:56 +0300

    задача на максимальное число

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write($"Число {num1} большее, а число {num2} меньшее");
+}
+else
+{
+    Console.Write($"Число {num2} большее, а число {num1} меньшее");
+}
