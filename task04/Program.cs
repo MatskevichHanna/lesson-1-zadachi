fc0d6f003e36d40c3cb7edd45d498f50381f5d17
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-04 23:14:18 +0300

    задача на максимальное из трех чисел

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max && num2 > num3)
+{
+    Console.Write($"Число {num2} максимальное");
+}
+else if (num3 > max && num3 > num2)
+{
+    Console.Write($"Число {num3} максимальное");
+}
+else
+{
+    Console.Write($"Число {num1} максимальное");
+}
