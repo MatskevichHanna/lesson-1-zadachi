431757b2bd204ebfbd2ab37cc263344654b0ef01
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-07 21:41:31 +0300

    задача на случайное число из отрезка

--- task09/Program.cs
+++ task09/Program.cs
@@ -5,15 +5,26 @@
 // 12-> 2
 // 85 -> 8
 
-int number = new Random().Next(10, 100);
-int firstDigit = number / 10;  //7
-int secondDigit = number % 10;  //8
+// int number = new Random().Next(10, 100);
+// int firstDigit = number / 10;  //7
+// int secondDigit = number % 10;  //8
 // if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
 // else Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
 
-if(firstDigit == secondDigit) Console.WriteLine($"Цифры равны");
-else
+// if(firstDigit == secondDigit) Console.WriteLine($"Цифры равны");
+// else
+// {
+//     int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+//     Console.WriteLine($"Наибольшая цифра числа {number} -> {max}");
+// }
+int number = new Random().Next(10, 100);
+
+int MaxDigit(int num)
 {
-    int max = firstDigit > secondDigit ? firstDigit : secondDigit;
-    Console.WriteLine($"Наибольшая цифра числа {number} -> {max}");
+    int firstDigit = num / 10;  //7
+    int secondDigit = num % 10;  //8
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа {number} -> {maxDigit}");
