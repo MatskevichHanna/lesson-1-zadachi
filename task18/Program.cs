5f94fd5580c76ca05082f4a995acea766af16c13
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-09 22:57:36 +0300

    задача на возможные координаты в четверти

--- task18/Program.cs
+++ task18/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc)
+{
+    if (x == 1) return "x>0, y>0";
+    if (x == 2) return "x<0, y>0";
+    if (x == 3) return "x<0, y<0";
+    if (x == 4) return "x>0, y<0";
+    return "Введите число от 1 до 4: ";
+}
+string result = Quarter(x);
+Console.WriteLine(result);
