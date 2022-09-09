1b3528760fd632ed1716598ae3ddb7a7436a4031
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-09 22:34:49 +0300

    задача на точку в плоскости

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Введите координаты точки: ");
+﻿// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+Console.WriteLine("Введите координаты точки: ");
 Console.WriteLine("X: ");
 int x = Convert.ToInt32(Console.ReadLine());
-Сonsole.WriteLine("Y: ");
+Console.WriteLine("Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
 string Quarter(int xc, int yc)
