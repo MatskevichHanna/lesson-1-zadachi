390b4e124aa72d6ed420bcc27b0bcaea1435fb15
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-10 23:22:23 +0300

    задача на реверс полиндрома

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstDigit = num / 10000 % 10;
+int secondDigit = num / 1000 % 10;
+int thirdDigit = num / 100 % 10;
+int fourthDigit = num / 10 % 10;
+int fiveDigit = num % 10;
+
+if (num > 9999 && num < 100000)
+{
+    if (firstDigit == fiveDigit && secondDigit == fourthDigit)
+    {
+        Console.WriteLine("Полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не полиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введите другое число: ");
+}
