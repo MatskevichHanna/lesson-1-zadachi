68e0add9e95b99e0a9b5e1c69a8cba00724d1268
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-10 23:54:04 +0300

    задача на таблицу кубов чисел

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+void Cube(int digit1, int count1)
+{
+    while (count1 <= digit1)
+    {
+        Console.WriteLine($"{count1} -> {count1 * count1 * count1,3}");
+        count1++;
+    }
+}
+Cube(digit, count);
