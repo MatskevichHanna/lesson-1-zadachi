520a46dcd2b6aec78a113af6609e7417e7c517b8
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-13 23:23:06 +0300

    задача на количество цифр в числе

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+    //     int i;
+    //     for (i = 1; i <= num; i ++)
+    //     {
+    //         num = num/10;
+    //     }
+    //     return i;
+    // }
+    // int result = Count(number);
+    // Console.WriteLine(result);
+    if (num != 0)
+    {
+        if (num < 0) num = -num;
+        int count = default;
+        while (num > 0)
+        {
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+    return 1;
+}
+int result = Count(number);
+Console.WriteLine(result);
