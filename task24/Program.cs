110cd8f84e013e3e7aa270ad2bcce6b5b2b42cf9
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-13 22:12:00 +0300

    задача на сумму чисел от 1 до А

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
