a586ba3a879c35574617c01911f92f15be4fd952
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-14 00:30:42 +0300

    задача на массив с нулями и единицами

--- task30/Program.cs
+++ task30/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+int[] arr = new int[8];
+
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+FillArray(arr);
+PrintArray(arr);
