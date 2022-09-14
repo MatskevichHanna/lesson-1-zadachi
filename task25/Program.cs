40148246cb563cca4e92ee2b50c4635ce0286f1b
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-15 00:10:54 +0300

    задача на возведение в степень натурального числа

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+double Power(int num, int deg)
+{
+    double power = 1;
+    if (deg >= 0)
+        for (int i = 0; deg > i; i++)
+        {
+            power = power * num;
+        }
+    else
+        for (int i = 0; deg < i; i--)
+        {
+            power = power / num;
+        }
+    return power;
+}
+double result = Power(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} равно {result}");
