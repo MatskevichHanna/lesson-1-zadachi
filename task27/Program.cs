11193e86bcb792e51c0568a6a0407cbdcace4e66
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-14 23:04:44 +0300

    задача на сумму цифр в числе

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+    if (num != 0)
+    {
+        if (num < 0) num = -num;
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    return 0;
+}
+
+int result = Count(number);
+Console.WriteLine($"Сумма цифр числа {number} = {result}");
