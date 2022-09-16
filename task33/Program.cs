a905d9ba13b95d1e7651c47fceaf785b6eda7ecf
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-16 23:40:36 +0300

    задача на присутствие числа в массиве

--- task33/Program.cs
+++ task33/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+bool ArraySearch(int[] array, int search1)
+{
+    bool exist = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == search1)
+        {
+            exist = true;
+            break;
+        }
+    }
+    return exist;
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10);
+PrintArray(arr);
+Console.WriteLine();
+Console.Write("Введите число для поиска в массиве: ");
+int search = Convert.ToInt32(Console.ReadLine());
+bool result = ArraySearch(arr, search);
+Console.WriteLine($"{result}");
