3f7a6f0789194ca25ec2f5d2a0f4773684b222c8
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-18 00:02:24 +0300

    задача на количество четных трехзначных чисел

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int RandomPositivNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) count += 1;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(10, 100, 1000);
+PrintArray(arr);
+int result = RandomPositivNumbers(arr);
+Console.WriteLine();
+int count = RandomPositivNumbers(arr);
+Console.WriteLine($"Количество четных чисел в массиве: {result} ");
