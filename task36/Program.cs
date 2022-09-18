b259c1b2078c6f4bd0fbe06c6ce9089079f7a719
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-18 21:35:05 +0300

    задача на сумму нечетных элементов массива

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumElemOddPosit(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0) sum += array[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArrayRndInt(5, 0, 100);
+PrintArray(arr);
+int result = SumElemOddPosit(arr);
+Console.WriteLine();
+int sum = SumElemOddPosit(arr);
+Console.WriteLine($" сумма элементов на нечётных позициях = {result}");
