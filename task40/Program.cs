c31a430b96e06b57d9fb7db4500c344b6b914da4
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-20 21:56:46 +0300

    задача на неравенство треугольника

--- task40/Program.cs
+++ task40/Program.cs
@@ -3,3 +3,19 @@
 // такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2)
+{
+    Console.WriteLine("Такой треугольник может существовать");
+}
+else
+{
+    Console.WriteLine("Такой треугольник не существует");
+}
