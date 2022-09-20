ad2b952e3ad3aa9a0d3ff91e07f1b05a9b3dd69e
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-20 23:34:51 +0300

    задача на преобразование десятичного числа в двоичное

--- task42/Program.cs
+++ task42/Program.cs
@@ -3,3 +3,27 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.Clear();
+int num = default;
+Console.Write("Введите целое число: ");
+while (!int.TryParse(Console.ReadLine(), out num))
+{
+    Console.Write("Ошибка ввода. Введите целое число: ");
+}
+
+
+int ConvertToBinary(int number)
+{
+    int result = default;
+    int count = 1;
+    while (number != 0)
+    {
+        result += (number % 2) * count;
+        number = number / 2;
+        count *= 10;
+    }
+    return result;
+}
+
+Console.Write(ConvertToBinary(num));
