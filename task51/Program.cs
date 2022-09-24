0177a6495edcfb5e4e787ace288bb1a372c4cf41
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-24 22:45:56 +0300

    задача на сумму элементов на диагонали

--- task51/Program.cs
+++ task51/Program.cs
@@ -1,3 +1,52 @@
 ﻿// Задайте двумерный массив. Найдите сумму
 // элементов, находящихся на главной диагонали (с индексами
 // (0,0); (1;1) и т.д.
+
+Console.Clear();
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumMidleMatrix(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) result += matrix[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+int sumResult = SumMidleMatrix(array2D);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, находящихся на главной диагонали равна {sumResult}");
