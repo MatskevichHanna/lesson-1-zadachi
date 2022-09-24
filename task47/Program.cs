7f005fa3c86538b190718eb0ad483e5b32a55bed
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-25 00:14:01 +0300

    задача на двумерный массив с вещественными числами

--- task47/Program.cs
+++ task47/Program.cs
@@ -4,3 +4,36 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max) + rnd.NextDouble();
+            matrix[i, j] = Math.Round(matrix[i, j], 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],6}, ");
+            else Console.Write($"{matrix[i, j],6} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2D = CreateMatrixRndInt(3, 4, -7, 17);
+PrintMatrix(array2D);
+
+
