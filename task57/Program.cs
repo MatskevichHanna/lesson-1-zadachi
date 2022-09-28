1120a572b55a3bc86b439dfa961803b0c59c6c9e
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-29 00:24:15 +0300

    задача на частотный словарь

--- task57/Program.cs
+++ task57/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] MatrixToArray(int[,] arr)
+{
+    int count = 0;
+    int[] array = new int[arr.Length];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            array[count] = arr[i, j];
+            count++;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+void ShowCount(int[] arr2)
+{
+    int num = arr2[0];
+    int count = 1;
+    for (int i = 1; i < arr2.Length; i++)
+    {
+        if(arr2[i] == num) count ++;
+        else
+        {
+            Console.WriteLine($"{num} содержится {count} раз");
+            num = arr2[i];
+            count = 1;
+        }
+        if(i == arr2.Length - 1) Console.WriteLine($"{num} содержится {count} раз");
+    }
+}
+
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] array = MatrixToArray(matrix);
+Array.Sort(array);
+PrintArray(array);
+Console.WriteLine();
+ShowCount(array);
