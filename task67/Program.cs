0c0a30b43672a7188aa3cdf32249ac154c18c07e
Matskevich_Hanna <hankohanna@mail.ru>
2022-10-10 22:53:26 +0300

    задача на рекурсию суммы цифр числа

--- task67/Program.cs
+++ task67/Program.cs
@@ -3,3 +3,30 @@
 // 453 -> 12
 // 45 -> 9
 
+Console.Clear();
+Console.Write($"Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NaturalNumbers(int num1)
+{
+    int sum = num1 % 10;
+    if(num1 != 0)
+    {
+        sum += NaturalNumbers(num1/10);
+    }
+    return sum;
+}
+
+int UserNumber(int number, string messange)
+{
+    Console.Write(messange);
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write($"Ошибка ввода. \n{messange}");
+    }
+    return number;
+}
+
+int summa = 0;
+summa = NaturalNumbers(num);
+Console.WriteLine(summa);
