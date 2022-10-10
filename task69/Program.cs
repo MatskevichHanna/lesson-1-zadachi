53b1169eaf44b8678153a504296dd7658645b579
Matskevich_Hanna <hankohanna@mail.ru>
2022-10-10 23:34:28 +0300

    задача на рекурсию степени числа

--- task69/Program.cs
+++ task69/Program.cs
@@ -2,3 +2,19 @@
 // возводит число А в целую степень B с помощью рекурсии.
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int DegreeOFNumber(int c, int d)
+{
+    if (d == 0) return 1;
+    return c *= DegreeOFNumber(c, d - 1);
+}
+
+int degree = DegreeOFNumber(a, b);
+Console.WriteLine(degree);
