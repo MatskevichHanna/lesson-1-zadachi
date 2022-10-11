0c70a2a15745f087726b7ef25536e5b500db0b22
Matskevich_Hanna <hankohanna@mail.ru>
2022-10-11 21:45:07 +0300

    задача на рекурсию суммы от M до N

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,3 +2,19 @@
 //которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write($"Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int NaturalNumbers(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    if (numM > numN) return numM + NaturalNumbers(numN, numM - 1);
+    else return numN + NaturalNumbers(numN - 1, numM);
+}
+
+int summa = NaturalNumbers(numberM, numberN);
+Console.WriteLine($"{summa}");
