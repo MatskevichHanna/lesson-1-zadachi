ece914cb61d178050b9b715204946b154a5887cc
Matskevich_Hanna <hankohanna@mail.ru>
2022-10-11 22:21:41 +0300

    задача на рекурсию функция Аккермана

--- task68/Program.cs
+++ task68/Program.cs
@@ -2,3 +2,22 @@
 //Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 0 || numberN < 0) Console.WriteLine($"Введите числа от 0 до 4");
+
+int AckermanFunction(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    else if (numM > 0 && numN == 0) return AckermanFunction(numM - 1, 1);
+    else if (numM > 0 && numN > 0) return AckermanFunction(numM - 1, AckermanFunction(numM, numN - 1));
+    else return numN + 1;
+}
+
+int result = AckermanFunction(numberM, numberN);
+Console.WriteLine($"{result}");
